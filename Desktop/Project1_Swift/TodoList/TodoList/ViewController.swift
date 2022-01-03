2ffed3eab339ac5c5c2059c3ddc10ad00949194f
haejii <yanghagi@naver.com>
2022-01-03 22:02:36 +0900

    Implement Alert

--- Desktop/Project1_Swift/TodoList/TodoList/ViewController.swift
+++ Desktop/Project1_Swift/TodoList/TodoList/ViewController.swift
@@ -9,11 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableButton: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func tapEditButton(_ sender: UIBarButtonItem) {
+    }
+    
+    @IBAction func tabAddButton(_ sender: UIBarButtonItem) {
+        let alert = UIAlertController(title: "할 일 등록", message: "할 일을 입력해주세요. ", preferredStyle: .alert)
+        let registerButton = UIAlertAction(title: "등록", style: .default, handler: { _ in
+            
+        })
+        let cancelButton = UIAlertAction(title: "취소", style: .cancel, handler: nil)
+        alert.addAction(cancelButton)
+        alert.addAction(registerButton)
+        self.present(alert, animated: true, completion: nil)
+    }
 }
 
